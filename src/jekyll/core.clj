8f2b1027d81ec270d541dcbec6bed163e6632f3b
Christian Weilbach <christian_weilbach@web.de>
2013-03-12 18:10:31 +0100

    Build compilation pipeline. Make example parser work.

--- src/jekyll/core.clj
+++ src/jekyll/core.clj
@@ -1,14 +1,38 @@
 (ns jekyll.core
-  (:use jekyll.parser))
+  (:use [jekyll.parser :only [parse]]))
 
-(defn call-stage
-  [stage input])
-
-(defn call-stages
-  [start-stage end-stage input])
-
-(defn all-stages
-  [input])
 
 (def stages
-  [])
+  "Stages of compilation."
+  [slurp parse])
+
+
+(defn- index-of [item coll]
+  (count (take-while (partial not= item) coll)))
+
+
+(defn- subrange
+  "Returns a inclusive range between start
+   and end in a vector."
+  [start end]
+  (subvec stages
+          (index-of start stages)
+          (inc (index-of end stages))))
+
+
+(defn call-stage
+  "Call a single stage on input."
+  [input stage]
+  (stage input))
+
+
+(defn call-stages
+  "Call compilation stages from start-stage to end-stage."
+  [start-stage end-stage input]
+  (reduce call-stage input (subrange start-stage end-stage)))
+
+
+(defn all-stages
+  "Run all stages and spit out bytecode."
+  [input]
+  (reduce call-stage input stages))
--- src/jekyll/parser.clj
+++ src/jekyll/parser.clj
@@ -1,6 +1,19 @@
 (ns jekyll.parser
-  (:use com.lithinos.amotoen.core :only [pegs lpegs]))
+  (:use [com.lithinos.amotoen.core :only [pegs lpegs pegasus wrap-string]] ))
 
-(def grammar {
-   :Document [:String :$]
-   :String (lpegs  '| "ab")})
+
+(def grammar
+  {
+   :Document ['(* :String) :$]
+   :String (lpegs  '| "ab")
+   })
+
+
+(defn parse
+  "Input string s."
+  [s]
+  (pegasus :Document grammar (wrap-string s)))
+
+
+(comment
+  (parse "ab"))
