5bf1e857c8cb1e967fa4ab1ed5d82b05dac008f3
RagingRanek <fuerstgivo@googlemail.com>
2013-03-12 15:36:43 +0100

    Add dependencies

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [com.lithinos/amotoen "0.2.0-SNAPSHOT"]
+                 [midje "1.4.0"])
