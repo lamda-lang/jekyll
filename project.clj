edb40adf3acc7c85238eab59ba033d81f65f89e9
RagingRanek <fuerstgivo@googlemail.com>
2013-03-13 16:50:09 +0100

    Added newer Midje version to dependencies

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [com.lithinos/amotoen "0.2.0-SNAPSHOT"]
-                 [midje "1.4.0"]])
+                 [midje "1.5.0"]])
