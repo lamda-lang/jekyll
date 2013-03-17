f2bf7fff488948a715299677eee5c791d5dc63ab
Christian Weilbach <christian_weilbach@web.de>
2013-03-17 04:42:43 +0100

    Implement first simple value literals. Range has weird parsing bug still.

--- src/jekyll/parser.clj
+++ src/jekyll/parser.clj
@@ -3,17 +3,44 @@
 
 
 (def grammar
-  {
-   :Document ['(* :String) :$]
-   :String (lpegs  '| "ab")
-   })
+  {:_* '(* (| \newline \return \tab \space))
+   :Namespace [ :_* '(* :Definition :_*) :$]
+   :Definition [ :Identity :_* \= :_* :Expression :_* ]
+   :Identity [ :Symbol ]
+   :Symbol '(* :Char)
+   :Char (lpegs  '| "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_")
+   :ExtendedChar (lpegs '| " -!@#$%^&*()+=[]{}\\|/?.,;:'<>")
+   :Expression '(| :Value :Range :Symbol)
+   :Value '(| :Keyword :Integer :Float :String :Nil :Boolean)
+   :Range '(| [ \. \. :Integer]
+              [:Integer \. \. :Integer]
+              [:Integer \. \.])
+   :String [\" '(* (| :Char :ExtendedChar)) \"]
+   :Keyword [\# :Symbol]
+   :Float ['(* :Digit) \. :Digit '(* :Digit)]
+   :Integer [:Digit '(* :Digit)]
+   :Digit (lpegs '| "0123456789")
+   :Nil (pegs "nil")
+   :Boolean '(| :True :False)
+   :True (pegs "true")
+   :False (pegs "false")})
 
+(name :$)
 
 (defn parse
   "Input string s."
   [s]
-  (pegasus :Document grammar (wrap-string s)))
+  (pegasus :Namespace grammar (wrap-string s)))
 
 
 (comment
-  (parse "ab"))
+  (parse "x = y")
+  (parse "a = 42")
+  (parse "s = \"Hello world!\"")
+  (parse "pi = 3.141")
+  (parse "b = .12345")
+  (parse "k = #keyword")
+  (parse "t = true")
+  (parse "f = false")
+  (parse "n = nil")
+  (parse "r1 = 1..99"))
