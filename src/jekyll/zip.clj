80d6a55d92311b4347d915ae106d29ba10634aa1
Konrad Kuehne <fuerstgivo@googlemail.com>
2013-04-14 13:55:17 +0200

    add tree finder

--- src/jekyll/zip.clj
+++ src/jekyll/zip.clj
@@ -42,3 +42,14 @@
         (do (println (zip/node loc) " results in " matcher-result)
           (recur (zip/next (zip/edit loc (partial editor matcher-result)))))
         (recur (zip/next loc))))))
+
+
+(defn tree-find
+  [zipper matcher]
+  (loop [loc zipper
+         node-list '()]
+    (if (zip/end? loc)
+      node-list
+      (if-let [matcher-result (matcher (zip/node loc))]
+        (recur (zip/next loc) (conj node-list (zip/node loc)))
+        (recur (zip/next loc) node-list)))))
