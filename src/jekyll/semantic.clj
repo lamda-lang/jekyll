d6278b80143bf1b409b0357ae697d32c78162d9f
Konrad Kuehne <fuerstgivo@googlemail.com>
2013-04-14 13:56:14 +0200

    experiment with tree reduction

--- src/jekyll/semantic.clj
+++ src/jekyll/semantic.clj
@@ -11,25 +11,42 @@
 
 (defn java-value [matched n]
   (if matched
-    (let [[k v] (first (:Value n))]
-      (case k
-        :String (stringify-token (map vals (second v)))
-;        :Nummeric (. Integer parseInt (stringify-token (map vals v))) ; only works for single digit atm.
-        :Numeric (. Integer parseInt (reduce str (map #(val (first %)) (flatten v))))
-        :Boolean (let [[bk bv] (first v)] (= :True bk))
-        :Nil nil
-        n))
+    (if (contains? n :Expression)
+      (let [[k v] (first (:Expression n))]
+        (case k
+          :String (stringify-token (map vals (second v)))
+                                        ;        :Nummeric (. Integer parseInt (stringify-token (map vals v))) ; only works for single digit atm.
+                                        ;        :Numeric (. Integer parseInt (reduce str (map #(val (first %)) (flatten v))))
+          :Boolean (let [[bk bv] (first v)] (= :True bk))
+          :Nil nil
+          n))
+      (reduce str (map #(val (first %)) (flatten (val (first n)))))
+      )
     n))
 
 
 (defn typify [clean-parse-tree]
   (tree-edit (universal-zip clean-parse-tree)
-             #(and (map? %) (contains? % :Value))
+             #(and (map? %) (or (contains? % :Identity) (contains? % :Expression)))
              java-value))
 
 
+(defn reduce-tree
+  [typified-parse-tree]
+  (vec (map #(:Definition (first %)) (first (:Module typified-parse-tree)))))
+
+
+(comment
   (-> "s = true \n h = \"Hello sailor!\" \n numBer=333 \n float = 234"
-      parse
-      clean-parse-tree
-      typify
-      )
+              parse
+              clean-parse-tree
+              typify
+              ))
+
+
+
+(-> "abc = \"xyz\" foo = \"bar\" z1 = a where a = 2 end "
+    parse
+    clean-parse-tree
+    typify
+    reduce-tree)
