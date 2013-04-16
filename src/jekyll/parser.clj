bccbfe74738a56429cc478f8df5b09c67893f655
Konrad Kuehne <fuerstgivo@googlemail.com>
2013-04-16 17:54:38 +0200

    update gramma

--- src/jekyll/parser.clj
+++ src/jekyll/parser.clj
@@ -1,23 +1,22 @@
 (ns jekyll.parser
   (:use [jekyll.pegasus :only [pegs lpegs pegasus wrap-string]]
-        [jekyll.semantic]
         [jekyll.zip]))
 
 
 (def grammar
   {:_* '(* :Whitespace)
    :Whitespace '(| \newline \return \tab \space)
-   :Module [ :_* '(* [:Definition :_* ])]
+   :Module [ :_* '(* [:Definition :_* ] :$)]
    :Definition [ :Identity :_* \= :_* :Expression :_* '(* :Scope)]
    :Scope [:Where :_* '(* :Definition) :_* :End :_*]
    :Identity [:Char '(* (| :Char :Digit))]
    :Char (lpegs  '| "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_")
    :SpecialChar (lpegs '| " -!@#$%^&*()+=[]{}\\|/?.,;:'<>")
-   :Expression '(| :Identifier :Range :Float :Integer :String :Nil :Boolean :List :Map :Set :Identity)
+   :Expression '(| :Identifier :Range :Float :Integer :String :Nil :Boolean :List :Map :Set :Identity) ; order is relevant
    :Integer [ :Digit '(* :Digit)]
    :Float [ :Integer \. :Integer]
    :Range ['(| :Float :Integer (* :Digit)) \. \. '(| :Float :Integer (* :Digit))]
-   :String [\" '(* (| :Char :SpecialChar)) \"]
+   :String [\" '(* (| :Char :SpecialChar :Digit)) \"]
    :Identifier [\# :Identity]
    :Digit (lpegs '| "0123456789")
    :Nil (pegs "nil")
@@ -39,12 +38,12 @@
 
 
 (defn parsing-artifact? [x]
-  (or (and (map? x) (or (contains? x :_*
-                                   )
+  (or (and (map? x) (or (contains? x :_*)
                         (contains? x :_+)))
       (= x '())
       (= x :$)
-      (= x \=)))
+      (= x \=)
+      (and (map? x) (or (contains? x :Where) (contains? x :End)))))
 
 
 (defn clean-parse-tree [parse-tree]
@@ -56,14 +55,3 @@
   (-> " x = false \n y = \"Hello World!\" \n"
       parse
       clean-parse-tree))
-
-(def tree
-  (-> "x = \"asfdadf\" y = true"
-              parse
-              clean-parse-tree))
-
-(map #(val (first %)) (tree-find (universal-zip tree) #(and (map? %) (contains? % :Definition))))
-
-(-> "x = y where y = 2 a = 1 end y = 7 a = 5"
-    parse
-    clean-parse-tree)
