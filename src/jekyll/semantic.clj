1606127ab380cb0e2641377e9c636f9d8d5200d4
jeth64 <jeth64@arcor.de>
2013-04-16 23:56:06 +0200

    Tree successfully reduced: str, symbol, int, float, bool, nil

--- src/jekyll/semantic.clj
+++ src/jekyll/semantic.clj
@@ -4,48 +4,38 @@
   (:require [clojure.zip :as zip]))
 
 
-(defn stringify-token [t]
-  (do (println (filter list? t))
-      (apply str (flatten t))))
+(defn stringify [t]
+  (apply str (flatten (map vals (flatten t)))))
 
 
-(defn java-value [matched n]
-  (if matched
-    (let [[mk mv] (first n)]
-      (case mk
-        :Identity (stringify-token (map vals (flatten mv)))
-        :Integer {:Integer (stringify-token (map vals (flatten mv)))}
-        ;:Float {:Float (stringify-token [(val (first mv)) \. (val (last mv))])}
-        :Expression (let [[k v] mv]
-                      (case k
-                        :String {:String (stringify-token (map vals (second v)))}
-                        :Integer mv
-                        :Float mv
-                                        ; :Range [(first v) (last v)]
-                        :Boolean (let [[bk bv] (first v)] (= :True bk))
-                        :Nil nil
-                        n))))
-     n))
+;(defn stringify-nums []
+
+  (defn reduce-tree [matched n]
+    (if matched
+      (let [mv (first (val (first n)))
+            [k v] mv]
+        (case k
+          :String {:String (stringify (second v))}
+          :Symbol (stringify v)
+          :Integer {:Integer (stringify v)}
+          :Float {:Float (stringify v)}
+          :Range {:Range [(stringify (val (first (first v)))) (stringify (val (first (last v))))]}
+          :Boolean {:Boolean (let [[bk bv] (first v)] (= :True bk))}
+          :Nil nil
+          n))
+      n))
 
 
-(defn typify [clean-parse-tree]
-  (tree-edit (universal-zip clean-parse-tree)
-             #(and (map? %) (or
-                             (contains? % :Identity)
-                             (contains? % :Integer)
-                             (contains? % :Float)
-                             (contains? % :Expression)))
-             java-value))
+  (defn typify [clean-parse-tree]
+    (tree-edit (universal-zip clean-parse-tree)
+               #(and (map? %) (or
+                               (contains? % :Identity)
+                               (contains? % :Expression)))
+               reduce-tree))
 
 
-(defn reduce-tree
-  [typified-parse-tree]
-  (vec (map #(:Definition (first %)) (first (:Module typified-parse-tree)))))
-
-
-
-  (-> "s = true \n h = \"Hello sailor!\" \n numBer=333 \n float = 2.34 "
-              parse
-              clean-parse-tree
-              typify
-              )
+  (-> "s = true \n h = \"Hello sailor!\" \n numBer=333 \n float = 2.34 \n range = 1..8 "
+      parse
+      clean-parse-tree
+      typify
+      )
