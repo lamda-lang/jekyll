d6755d66fbd9b90ec7681556c2001a80771c29d2
Christian Weilbach <christian_weilbach@web.de>
2013-05-21 22:05:06 +0200

    Generate first runnable byte-code of a simple lamda.

--- src/jekyll/bytecode.clj
+++ src/jekyll/bytecode.clj
@@ -8,11 +8,48 @@
 (defn vbri [i] ; works only for < 128 now
   i)
 
-(defn create-string [s]
-   (concat [3 (.length s)] (.toCharArray s)))
+#_(2 2 4 1 1 0 15 0 8 12 \H \e \l \l \o \space \W \o \r \l \d \! 0 1 9 1 0 0)
+(defn create-string [index s]
+  (concat [8 (vbri (.length s))] (.toCharArray s) [(vbri index)]))
+
+
+(defn create-lambda [index arity code]
+  (concat  [(vbri 4)                       ; opcode for lamda
+            (vbri 1)                       ; register count for lambda TODO
+            (vbri 1)                       ; instruction count TODO
+            (vbri 0)                       ; context count TODO
+            (vbri (count (map byte code))) ; code size
+            (vbri arity)]
+           code
+           [(vbri index)]))
+
+
+; add lambda for identity
+(defn apply-lambda [target arity args result]
+  (concat [(vbri 9) ; opcode for apply
+           (vbri target)
+           (vbri arity)]
+           args
+           [(vbri result)]))
+
+
 
 (comment
   (write-bytes-to-file "/tmp/test"
-                       (map byte (concat [(vbri 0)  ; register count
-                                          (vbri 0)] ; instruction count
-                                         (create-string "Hello World!")))))
+                       (map byte
+                            (let [s (create-string 0 "Hello World!")
+                                  l (create-lambda 1 0 s)
+                                  a (apply-lambda 1 0 [] 0)]
+                              (concat [(vbri 2)  ; register count
+                                       (vbri 2)] ; instruction count
+                                      l
+                                      a)))))
+
+(byte-array (map byte
+                 (let [s (create-string 0 "Hello World!")
+                       l (create-lambda 1 0 s)
+                       a (apply-lambda 1 0 [] 0)]
+                   (concat [(vbri 2)  ; register count
+                            (vbri 2)] ; instruction count
+                           l
+                           a))))
