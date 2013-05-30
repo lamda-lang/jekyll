3aa7429ab93a2fcf11649a447bead28fea796a19
Konrad Kuehne <konrad.kuehne@rocketmail.com>
2013-05-30 16:01:55 +0200

    export grammar to file

--- src/jekyll/parser.clj
+++ src/jekyll/parser.clj
@@ -2,37 +2,8 @@
   (:require [instaparse.core :as insta]))
 
 (def parser
-  (insta/parser
-   "Module = <_*> Definition* <_*>
-    Definition = Identity <_*> <'='> <_*> Expression <_*> Scope*
-    Scope = <'where'> <_*> (Definition <_*>)* <'end'> <_*>
-    Identity = !('nil' | 'true' | 'false' | 'when' | 'case') #'[A-Za-z_]\\w*'
-    Expression = Identifier | Range | Integer | String | Nil | Boolean | Application | Lambda | List | Map | Set | Identity | Condition | Case | Property | Subscript | Float
-    Expressions = (Expression <_+>)* Expression
-    KeyValue = Expression <_*> <':'> <_*> Expression
-    KeyValues = (KeyValue <_+>)* KeyValue
-    Application = ( Identity | Application | Lambda ) <'('>  <_*> [Expressions <_*>] <')'>
-    Lambda = <'('> <_*> Args <':'> <_*> Expressions <_*> <')'>
-    Condition = <'when'> <_*> (KeyValue <_*>)* <'end'>
-    Case = <'case'> <_*> Identity <_+> (Expression <_*> [Guard <_*>] <':'> <_*> Expression <_*>)* <'end'>
-    Property = Expression <'.'> Expression
-    Guard = <'if'> <_*> Expressions
-    Subscript = Expression <'['> <_*> Expression <_*> <']'>
-    ListComprehension = Expression <_*> <'for'> <_*> KeyValue <_*> [Guard]
-    MapComprehension = KeyValue <_*> <'for'> <_*> KeyValue <_*> [Guard]
-    Args = (Identity <_*>)*
-    Float = #'-?\\d+.\\d+'
-    Range = ( Expression | Epsilon ) '..' ( Expression | Epsilon )
-    Integer = #'-?\\d+'
-    String = <'\\\"'> #'[^\"]*' <'\\\"'>
-    Identifier = <'#'> #'\\w+'
-    Nil = <'nil'>
-    Boolean = 'true' | 'false'
-    List = <'['> <_*> ( Expressions | ListComprehension )? <_*> <']'>
-    Map =  <'{'> <_*> ( KeyValues   | MapComprehension  )? <_*> <'}'>
-    Set =  <'('> <_*> ( Expressions | ListComprehension )? <_*> <')'>
-    _ = #'\\s'"))
-
+  "Create parser with given grammar file"
+  (insta/parser "doc/grammar"))
 
 
 (defn parse [s]
