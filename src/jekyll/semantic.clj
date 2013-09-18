72e78bdc98aa23fe3576f8b2ab6f2fd08218be86
jeth64 <jeth64@arcor.de>
2013-09-18 17:18:09 +0200

    Update file for bytecode

--- src/jekyll/semantic.clj
+++ src/jekyll/semantic.clj
@@ -2,7 +2,8 @@
   (:use [jekyll.zip]
         [jekyll.parser]
         [clojure.inspector :only [inspect-tree]]
-        [jekyll.bytecode])
+        [jekyll.bytecode]
+        [jekyll.bitsnbytes])
   (:require [clojure.zip :as zip]))
 
 
@@ -68,14 +69,13 @@
                                 (= (first %) :Expression)))
              typify-definition))
 
-(defn expression2bytes [matched n]
+(defn- expression2bytes [matched n]
   (if matched
     (if (= :Expression (first n))
       (let [[k & vs] (second n)
             [v] vs]
         (case k
-;          :String (str2bin v)
-;          :Variable (id2bin v)
+          :String (str2bin v)
 ;          :Token (token2bin v)
           :Integer (int2bin (. Integer parseInt v))
           :Float (float2bin (. Float parseFloat v))
@@ -94,34 +94,26 @@
 ;          :Range (rng2bin (first vs) (last vs))
 ;          :Protocol (protocol2bin v)
 ;          :Type (type2bin v)
-          n))
-      (id2bin (second n)))
+          (rest n)))
+      (id2bin (rest n)))
     n))
 
 (defn binarize-expressions [clean-parse-tree]
   (tree-edit (universal-zip clean-parse-tree)
              #(and (vector? %) (or
-                                (= (first %) :Variable)
+                                (= (first %) :Identifier)
                                 (= (first %) :Expression)))
              expression2bytes))
 
 (defn join-bytes
   "Turns a tree of byte-arrays into one huge byte-array"
   [tree]
-  (join-byte-arrays (flatten tree)))
+  (apply join-byte-arrays (flatten tree)))
 
-(-> "res = func(2)"
+(-> "a = b"
            parse
            collapse
            binarize-expressions
-;           typify
-;           reduce-collections
-;           lisp-case
-;           lisp-when
-;           lisp-apply
-;           lisp-eval
-;           lisp-where
-;           lisp-def
            )
 
 (defn simplify-collection [matched n]
